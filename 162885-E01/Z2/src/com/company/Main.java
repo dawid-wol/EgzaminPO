7cc7bfa2d006a786a961c15e9f3a3819d780c928
Dawid <bigcoconut01@gmail.com>
2022-02-01 11:16:30 +0100

    Initial commit

--- 162885-E01/Z2/src/com/company/Main.java
+++ 162885-E01/Z2/src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<String> list = new ArrayList<String>();
+        list.add("aaa");
+        list.add("bbb");
+        list.add("ccc");
+
+        printMarginal(list);
+
+        List<String> listInt = new LinkedList<String>();
+        listInt.add("zzzzzzzzzzz");
+        listInt.add("aaaaaaasd");
+
+        printMarginal(listInt);
+    }
+    public static <E> void printMarginal(Iterable<E> object){
+        for(E e: object){
+        System.out.println(e);
+        }
     }
 }
