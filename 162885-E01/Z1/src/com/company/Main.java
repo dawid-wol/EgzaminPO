3cbf7884fbb3e049129f6b8fde7433b212736fc8
Dawid <bigcoconut01@gmail.com>
2022-02-01 10:35:48 +0100

    Initital Commit

--- 162885-E01/Z1/src/com/company/Main.java
+++ 162885-E01/Z1/src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Character[] characterRepeated = {'a','b','a','c','d','a' };
+        LocalTime[] localTimesRepeated = {
+                LocalTime.of(12,13),
+                LocalTime.of(14,55),
+                LocalTime.of(12,13),
+                LocalTime.of(15,33)
+        };
+        System.out.println(ArrayUtil.removeRepeatedElements(characterRepeated));
+        System.out.println(ArrayUtil.removeRepeatedElements(localTimesRepeated));
+    }
+}
+
+class ArrayUtil{
+    public static <T extends Comparable<T>> ArrayList<T> removeRepeatedElements(T[] array) {
+        ArrayList<T> list = new ArrayList<>();
+        for (T t : array) {
+            for (T value : array) {
+                if (t != value && !list.contains(t)) {
+                    list.add(t);
+                }
+            }
+        }
+        return list;
     }
 }
